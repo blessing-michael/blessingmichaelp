d7bf6c9d970a7c7ae7bc4f5ce7ed7814d89d0a6a
blessing-michael <blessingmike334@gmail.com>
2023-05-08 11:20:17 -0700

    action added to form element in contact component

--- src/Contact.js
+++ src/Contact.js
@@ -1,28 +1,38 @@
-import React from 'react'
-import {BsSend } from "react-icons/bs";
+import React from "react";
+import { BsSend } from "react-icons/bs";
 // import {AiFillGithub, AiFillLinkedin, AiFillTwitterCircle } from "react-icons/ai";
 // import {FaDev } from "react-icons/fa";
 
-
 function Contact() {
   return (
-   <section className='contact' id='contact'>
-    <p className='contact-p'>what's next?</p>
-    <h2  className='contact-h2'>Get in touch</h2>
-    <p  className='contact-p'>I'm interested in internship opportunities- especially opportunities that involves large projects. However, my inbox is always open if you have any questions or want to say hi.</p>
-    <div className="form">
-        <form name='contact' method='post'>
-        <input type="hidden" name="form-name" value="contact" />
-            <input type="text"  placeholder='Your Name'/>
-            <input type="email"  placeholder='Your Email'/>
-            <textarea className='textarea' name="" id="" cols="30" rows="10" placeholder='Your Message' ></textarea>
-            <button className='contact-btn'>Send Message <BsSend/></button>
+    <section className="contact" id="contact">
+      <p className="contact-p">what's next?</p>
+      <h2 className="contact-h2">Get in touch</h2>
+      <p className="contact-p">
+        I'm interested in internship opportunities- especially opportunities
+        that involves large projects. However, my inbox is always open if you
+        have any questions or want to say hi.
+      </p>
+      <div className="form">
+        <form name="contact" method="post" action="/contact">
+          <input type="hidden" name="form-name" value="contact" />
+          <input type="text" id="name" name="name" placeholder="Your Name" />
+          <input type="email" placeholder="Your Email" name="email" />
+          <textarea
+            className="textarea"
+            name="message"
+            id="message"
+            cols="30"
+            rows="10"
+            placeholder="Your Message"
+          ></textarea>
+          <button className="contact-btn" type="submit">
+            Send Message <BsSend />
+          </button>
         </form>
-    </div>
- 
-    
-   </section>
-  )
+      </div>
+    </section>
+  );
 }
 
-export default Contact
+export default Contact;
