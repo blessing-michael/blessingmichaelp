3ee62743d58982ebaae0bb081dd22ec5ba802070
Blessing <blessingmike334@gmail.com>
2023-10-07 11:22:39 +0100

    Add cv link to portfolio

--- src/Header.js
+++ src/Header.js
@@ -10,6 +10,15 @@ function Header() {
             <h1>I'm Blessing</h1>
             <h1> Front-End Developer</h1>
             <button className='cv-btn'>Cv/Resume</button>
+            <a
+            className="footer-a nav-a"
+            href="https://docs.google.com/document/d/1Z2JSFpLZi3EJeBFBurkhdVcw5LoxzSnQhV2L3Q6lfJk/edit?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Cv/Resume
+          </a>
+            
         </header>
         <section className='portfolio'>
             <div className="portfolio-info">
